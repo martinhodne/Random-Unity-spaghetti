d0712e87d07b00d5458806034166199e8744033d
martinhodne <mahodne@gmail.com>
2015-03-03 19:47:12 +0100

    Unity rakett prosjekt
    kuk i kano

---
+++ New Unity Project 1/Assets/Scripts/RocketController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketController : MonoBehaviour {
+
+	public float flySpeed = 5;
+
+
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.Space))
+		{
+			rigidbody2D.AddForce(Vector3.up * flySpeed);
+		}
+	}
+}
